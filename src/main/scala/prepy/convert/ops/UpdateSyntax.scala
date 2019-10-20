c0632dbf4a8651b71160f2c464135ba86fa9c3d9
alexandrustana <alexandrustana2@gmail.com>
2019-10-20 23:38:08 +0300

    WIP insert/update/delete syntax

---
+++ src/main/scala/prepy/convert/ops/UpdateSyntax.scala
@@ -0,0 +1,21 @@
+package prepy.convert.ops
+
+import prepy.convert.Domain
+import shapeless.Typeable
+
+object UpdateSyntax {
+
+  def select[T <: Product](implicit inst: Domain[T]): SelectElem = SelectElem(inst.fields)
+
+  case class SelectElem(fields: List[Symbol]) {
+    def from[T](implicit typeable: Typeable[T]) = WhereElem(fields, typeable.describe)
+  }
+
+  case class WhereElem(fields: List[Symbol], tableName: String) {
+
+    def apply(): String = s"SELECT ${fields.map(_.name).mkString(",")} FROM $tableName"
+
+    def where(condition: String*): String =
+      s"SELECT ${fields.map(_.name).mkString(",")} FROM $tableName WHERE ${condition.mkString("(", "AND", ")")}"
+  }
+}
