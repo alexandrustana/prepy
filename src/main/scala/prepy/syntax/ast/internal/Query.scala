b8820e82161cdb9d3abfec3cebd5227eec56e725
alexandrustana <alexandrustana2@gmail.com>
2019-10-31 12:13:19 +0200

    Change access specifier for QueryElement

--- src/main/scala/prepy/syntax/ast/internal/Query.scala
+++ src/main/scala/prepy/syntax/ast/internal/Query.scala
@@ -3,6 +3,7 @@ package prepy.syntax.ast.internal
 import cats.data.Validated
 import cats.data.Validated.Valid
 
-private[syntax] trait Query { self =>
+private[internal] trait Query { self =>
+
   def apply(): Validated[String, String] = Valid(self.toString)
 }
