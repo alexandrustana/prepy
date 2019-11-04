d6d22aedc2d2cfebc9c04e659e3e3fbaa4b16a42
alexandrustana <alexandrustana2@gmail.com>
2019-11-04 16:06:15 +0200

    Fix issue which prevents injecting updatable object

--- src/main/scala/prepy/syntax/doobie/internal/Insert.scala
+++ src/main/scala/prepy/syntax/doobie/internal/Insert.scala
@@ -7,7 +7,7 @@ import prepy.syntax.ast.internal.{Insert => internal}
 private[doobie] trait Insert {
 
   implicit class `valueD`[O](elem: internal.`valuesT`[_] { type Out = O })(implicit write: Write[O]) {
-    def update(): Update0 = Update0(elem.toString, None)
+    def update(): doobie.Update[O] = doobie.Update[O](elem.toString, None)
   }
 
 }
--- src/main/scala/prepy/syntax/doobie/internal/Update.scala
+++ src/main/scala/prepy/syntax/doobie/internal/Update.scala
@@ -8,7 +8,7 @@ import prepy.syntax.ast.internal.{Update => internal}
 private[doobie] trait Update {
 
   implicit class `setD`[O <: Product](elem: internal.`setT`[_] { type Out = O })(implicit write: Write[O]) {
-    def update(): Update0 = Update0(elem.toString, None)
+    def update(): doobie.Update[O] = doobie.Update[O](elem.toString, None)
 
     def where(fr: Fragment): Update.`whereD`[O] = Update.`whereD`[O](elem, fr)
   }
@@ -16,7 +16,7 @@ private[doobie] trait Update {
   implicit class `updateFilterD`[O <: Product](elem: Update.logicalOpD[_] { type Out = O })(
     implicit write:                                  Write[O]
   ) {
-    def update(): Update0 = elem.compile().update
+    def update(): doobie.Update[O] = doobie.Update[O](elem.compile().update.sql, None)
   }
 }
 
