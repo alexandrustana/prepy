4411972e75080d60db97601d4bfa7c9a8c02a188
alexandrustana <alexandrustana2@gmail.com>
2019-11-10 14:42:48 +0200

    WIP insert join syntax

--- src/main/scala/prepy/syntax/ast/internal/Select.scala
+++ src/main/scala/prepy/syntax/ast/internal/Select.scala
@@ -3,7 +3,8 @@ package prepy.syntax.ast.internal
 import cats.data.Validated.Invalid
 import prepy.formatter.{Formatter, IdentityFormatter}
 import prepy.implicits.Implicits._
-import shapeless.Typeable
+import shapeless.ops.hlist.IsHCons
+import shapeless.{:+:, CNil, Coproduct, HList, HNil, Typeable}
 
 private[syntax] trait Select {
 
@@ -18,21 +19,39 @@ object Select extends Where {
       Invalid("Incomplete SQL query. `select[T]` must be followed by a `from[K]`")
 
     def from[K <: Product](implicit typeable: Typeable[K], transform: Transform[K, T]): Select.`fromT`[T] =
-      `fromT`[T](this, typeable.describe, formatter)
+      `fromT`[T, K](this, typeable.describe, formatter)
 
     override def toString: String = s"SELECT ${fields.map(_.name).map(formatter.apply).mkString(", ")}"
   }
 
-  private[syntax] case class `fromT`[T <: Product](
-    queryElement: Query,
-    tableName:    String,
-    formatter:    Formatter
+  private[syntax] case class `fromT`[O <: Product, T <: Product](
+    elem:      Query,
+    table:     String,
+    formatter: Formatter
   ) extends Query {
-    type Out = T
+    type Out = O
 
-    def where(condition: String): `whereT`[T] = `whereT`[T](this, condition)
+    def where(condition: String): `whereT`[O, T] = `whereT`[O, T](this, condition)
 
-    override def toString: String = s"$queryElement FROM ${formatter(tableName)}"
+    def join[K <: Product]: `joinT`[Out, T :+: K :+: CNil] =
+      `joinT`[O, T :+: K :+: CNil](elem, formatter)
+
+    override def toString: String = s"$elem FROM ${formatter(table)}"
   }
 
+  private[syntax] case class `joinT`[O, T <: Coproduct](elem: Query, formatter: Formatter) extends Query {
+    type Out = O
+
+    def on(f: T => Boolean): `onT`[Out, T] = `onT`[Out, T](elem, f, formatter)
+  }
+
+  private[syntax] case class `onT`[O, T <: Coproduct](elem: Query, f: T => Boolean, formatter: Formatter)
+      extends Query {
+    type Out = O
+
+    def where(condition: String): `whereT`[O, T] = `whereT`[O, T](this, condition)
+
+    def join[K <: Product]: `joinT`[O, K :+: T] =
+      `joinT`[O, K :+: T](elem, formatter)
+  }
 }
