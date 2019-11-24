8e502097f2b4a10ec2250beeadf40ccdacc9e7fd
alexandrustana <alexandrustana2@gmail.com>
2019-11-24 19:35:58 +0200

    Finish first version of ast for available operators

--- syntax/src/main/scala/prepy/syntax/Operation.scala
+++ syntax/src/main/scala/prepy/syntax/Operation.scala
@@ -2,37 +2,41 @@ package prepy.syntax
 
 import spire.math._
 
-trait Component[T] {}
+trait Component[T] {
+  type Out = T
+}
 case class Constant[T](t:    T)      extends Component[T]
 case class Variable[T](name: String) extends Component[T]
 
 trait Expression[I, O] extends Component[I] {}
 
-abstract class ArithmeticOperation[T: Numeric] extends Expression[T, T] {}
+abstract class ArithmeticOperation[T: Numeric] extends Component[T] {}
 
-case class Add[T: Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
+case class Add[T:      Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
 case class Subtract[T: Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
 case class Multiply[T: Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
-case class Divide[T: Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
-case class Modulo[T: Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
+case class Divide[T:   Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
+case class Modulo[T:   Numeric](operand1: Component[T], operand2: Component[T]) extends ArithmeticOperation[T]
 
-abstract class BitwiseOperation[T: Integral] extends Expression[T, T] {}
+abstract class BitwiseOperation[T: Integral] extends Component[T] {}
 
-case class And[T:         Integral] private (operand1: Component[T], operand2: Component[T]) extends BitwiseOperation[T]
-case class Or[T:          Integral] private (operand1: Component[T], operand2: Component[T]) extends BitwiseOperation[T]
+case class BitAnd[T:         Integral] private (operand1: Component[T], operand2: Component[T]) extends BitwiseOperation[T]
+case class BitOr[T:          Integral] private (operand1: Component[T], operand2: Component[T]) extends BitwiseOperation[T]
 case class ExclusiveOr[T: Integral] private (operand1: Component[T], operand2: Component[T]) extends BitwiseOperation[T]
 
-trait ComparisonOperator[T] extends Expression[T, Boolean] {}
+trait ComparisonOperator extends Component[Boolean] {}
 
-case class Eq[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator[T]
-case class Gt[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator[T]
-case class Lt[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator[T]
-case class Gte[T](operand1: Component[T], operand2: Component[T]) extends ComparisonOperator[T]
-case class Lte[T](operand1: Component[T], operand2: Component[T]) extends ComparisonOperator[T]
+case class Eq[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator
+case class Gt[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator
+case class Lt[T](operand1:  Component[T], operand2: Component[T]) extends ComparisonOperator
+case class Gte[T](operand1: Component[T], operand2: Component[T]) extends ComparisonOperator
+case class Lte[T](operand1: Component[T], operand2: Component[T]) extends ComparisonOperator
 
-object Demo {
-  Add(Add(Constant(1), Constant(2)), Constant(3))
-  And(Constant(1), Constant(2))
-  Eq(Eq(Constant(3),  Add(Constant(1), Constant(2))), Constant(1))
-  Add(Variable("a"), Constant(3))
-}
+trait LogicalOperator extends Component[Boolean] {}
+
+case class LogicalAnd[T](operand1:     Component[T], operand2: Component[T]) extends LogicalOperator
+case class LogicalOr[T](operand1:      Component[T], operand2: Component[T]) extends LogicalOperator
+case class Between[T](operand1: Component[T], operand2: Component[T], operand3: Component[T]) extends LogicalOperator
+case class In[T](operand1:      Component[T], operand2: List[Component[T]]) extends LogicalOperator
+case class Like[T](operand1:    Component[T], operand2: Component[T]) extends LogicalOperator
+case class Not[T](operand1:     Component[T], operand2: Component[T]) extends LogicalOperator
