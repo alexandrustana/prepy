ddbd9ee73a448b24c4250fe6cc52371420416918
alexandrustana <alexandrustana2@gmail.com>
2019-11-25 23:14:05 +0200

    WIP macros for sql operations

---
+++ syntax/src/main/scala/prepy/syntax/Macros.scala
@@ -0,0 +1,37 @@
+package prepy.syntax
+import scala.language.experimental.macros
+import scala.reflect.api.Trees
+import scala.reflect.macros.blackbox
+
+object Macros {
+
+  private def getComponent[T](
+    tree:       Trees#Tree
+  )(implicit c: blackbox.Context): Component = {
+    import c.universe._
+    tree match {
+      case Select(_, TermName(x)) => Variable(x)
+      case Literal(Constant(a))   => Value(a)
+      case q"$a == $b"            => Eq(getComponent(a), getComponent(b))
+      case q"$a != $b"            => Neq(getComponent(a), getComponent(b))
+      case q"$a > $b"             => Gt(getComponent(a), getComponent(b))
+      case q"$a >= $b"            => Gte(getComponent(a), getComponent(b))
+      case q"$a < $b"             => Lt(getComponent(a), getComponent(b))
+      case q"$a <= $b"            => Lte(getComponent(a), getComponent(b))
+      case q"$a && $b"            => LogicalAnd(getComponent(a), getComponent(b))
+      case q"$a || $b"            => LogicalOr(getComponent(a), getComponent(b))
+    }
+  }
+
+  def stringify[T <: Product](f: T => Boolean): String = macro impl[T]
+
+  def impl[T: c.WeakTypeTag](c: blackbox.Context)(f: c.Expr[T => Boolean]): c.Expr[String] = {
+    import c.universe._
+
+    val Function(List(ValDef(_, name, _, _)), funcBody) = f.tree
+
+    val components = getComponent[T](funcBody)(c)
+
+    c.Expr[String](q"${components.stringify(c)}")
+  }
+}
